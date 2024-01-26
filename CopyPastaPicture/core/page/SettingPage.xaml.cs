294bf5f3543416889baf3959f1eb5595ad936ce7
Jo Matsumaru <hatunehimeka@outlook.jp>
2024-01-27 04:35:19 +0900

    設定システムのコード修正

--- CopyPastaPicture/core/page/SettingPage.xaml.cs
+++ CopyPastaPicture/core/page/SettingPage.xaml.cs
@@ -12,6 +12,7 @@ public partial class SettingPage : Page
 {
     TomlControl _tomlControl = new();
     LogController _logController = new();
+    String _initLanguage = "";
     public SettingPage()
     {
         InitializeComponent();
@@ -39,6 +40,55 @@ public partial class SettingPage : Page
                 JapaneseComboBoxItem.Content = JaLanguage.LanguageJapanese;
                 break;
         }
+
+        _initLanguage = _tomlControl.LanguageName();
+    }
+
+    private void SaveContent()
+    {
+        //言語設定
+        switch (LanguageCombo.SelectedIndex)
+        {
+            case 0:
+                TomlEditLanguage("en-US");
+                Console.WriteLine("en-US");
+                break;
+            case 1:
+                TomlEditLanguage("ja-JP");
+                Console.WriteLine("ja-JP");
+                break;
+        }
+        
+        //セーブ処理後の処理
+        LanguageLoaded();
+        ((App)Application.Current).LoadNotifyIcon();
+        _logController.InfoLog("JapaneseComboBoxItem Success");
+    }
+
+    private int CheckSavedItem()
+    {
+        int result;
+        string language = "";
+        // 言語変更確認
+        switch (LanguageCombo.SelectedIndex)
+        {
+            case 0 :
+                language = "en-US";
+                break;
+            case 1:
+                language = "ja-JP";
+                break;
+        }
+
+        if (_initLanguage == language)
+        {
+            result = 0;
+        }
+        else
+        {
+            result = 1;
+        }
+        return result;
     }
     
     private void TomlEditLanguage(string data)
@@ -64,38 +114,6 @@ public partial class SettingPage : Page
         }
     }
 
-    private void EnglishComboBoxItem_OnSelected(object sender, RoutedEventArgs e)
-    {
-        try
-        {
-            TomlEditLanguage("en-US");
-            LanguageLoaded();
-            ((App)Application.Current).LoadNotifyIcon();
-            _logController.InfoLog("EnglishComboBoxItem Success");
-        }
-        catch (Exception exception)
-        {
-            _logController.ErrorLog($"EnglishComboBoxItem Error{exception}");
-            throw;
-        }
-    }
-
-    private void JapaneseComboBoxItem_OnSelected(object sender, RoutedEventArgs e)
-    {
-        try
-        {
-            TomlEditLanguage("ja-JP");
-            LanguageLoaded();
-            ((App)Application.Current).LoadNotifyIcon();
-            _logController.InfoLog("JapaneseComboBoxItem Success");
-        }
-        catch (Exception exception)
-        {
-            _logController.ErrorLog($"JapaneseComboBoxItem Error{exception}");
-            throw;
-        }
-    }
-
     private void LanguageCombo_OnLoaded(object sender, RoutedEventArgs e)
     {
         switch (_tomlControl.LanguageName())
@@ -107,5 +125,68 @@ public partial class SettingPage : Page
                 LanguageCombo.SelectedItem = JapaneseComboBoxItem;
                 break;
         }
+    }
+
+    private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+         if (CheckSavedItem() == 1)
+         {
+             _logController.InfoLog("Cancel SettingPage Success");
+             switch (_tomlControl.LanguageName())
+             {
+                    case "ja-JP":
+                        var resultJa = MessageBox.Show("変更されている設定があります、本当にキャンセルしますか？", "CopyPastaPicture",
+                            MessageBoxButton.YesNo, MessageBoxImage.Question);
+                        switch (resultJa)
+                        {
+                            case MessageBoxResult.No:
+                                return;
+                            case MessageBoxResult.Yes:
+                            {
+                                var parentWindowJa = Window.GetWindow(this);
+                                parentWindowJa?.Close();
+                                break;
+                            }
+                        }
+                        break;
+                        case "en-US":
+                        var resultEn = MessageBox.Show("There are some settings that have been changed, are you sure you want to cancel?", "CopyPastaPicture",
+                            MessageBoxButton.YesNo, MessageBoxImage.Question);
+                        switch (resultEn)
+                        {
+                            case MessageBoxResult.No:
+                                return;
+                            case MessageBoxResult.Yes:
+                            {
+                                var parentWindowEn = Window.GetWindow(this);
+                                parentWindowEn?.Close();
+                                break;
+                            }
+                        }    
+                            break;
+             }
+         }
+         else
+         {
+                _logController.InfoLog("Cancel SettingPage Success");
+                var parentWindow = Window.GetWindow(this);
+                if (parentWindow != null)
+                {
+                    parentWindow.Close();
+                }
+         }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw;
+        }
+    }
+
+    private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        SaveContent();
     }
 }
